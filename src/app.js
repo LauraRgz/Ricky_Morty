62ef3db77410710d45f3212d68c55a97c136fcce
LauraRgz <lrodriguezs5@alumnos.nebrija.es>
2019-10-17 17:49:06 +0200

    Parte3

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ const runApp = data => {
       planet: String
     }
     type Query{
-      character(id: Int!): Character 
+      character(id: Int!): Character
+      characters(page: Int!, pageSize: Int!): [Character]
     }
   `
   const resolvers = {
@@ -35,15 +36,40 @@ const runApp = data => {
           }
         }else return null;
 
+      },
+      characters: (parent, args, ctx, info) =>{
+        const pagina = args.page || 1;
+        const paginaSize = args.pageSize || 20;
+        const inicio =((pagina-1)* paginaSize);
+        const fin = (pagina*paginaSize);
+        const array = data.slice(inicio, fin);
+          return array.map(character => {
+            return {
+              id: character.id,
+              name: character.name,
+              status: character.status,
+              planet: character.location.name
+            }
+          });
+        }
+        // if(result){
+        //   return {
+        //     id: result.id,
+        //     name: result.name,
+        //     status: result.status,
+        //     planet: result.location.name
+        //   }
+        // }
       }
     }
-  }
   const server = new GraphQLServer({typeDefs, resolvers});
   server.start();
-    // data.forEach(element => {
+  }
+  
+  // data.forEach(element => {
   //   console.log(`${element.id}: ${element.name}`);
   // });
-};
+
 
 // main program
 fetchData(runApp, url);
